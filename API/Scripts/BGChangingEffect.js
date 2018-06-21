ebee2d8fb13cd3ff6988b23cc7d70543727d290a
australiaspolocies <40449336+australiaspolocies@users.noreply.github.com>
2018-06-22 09:33:10 +1000

    Create BGChangingEffect.js

---
+++ API/Scripts/BGChangingEffect.js
@@ -0,0 +1,12 @@
+function StartUp()
+{
+  this.BG-color = [200, 200, 300];
+  changeBG();
+}
+function changeBG()
+{
+ while(true)
+ {
+  document.body.style.background-color = rgba(BG-color[0]++, BG-color[1]++, BG-color[2]++, 0.3);
+ }
+}
