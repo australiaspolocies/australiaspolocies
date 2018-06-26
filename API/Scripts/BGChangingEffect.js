8d6b9ca5c0821198e29471a3402a07c8ceea5ff3
australiaspolocies <40449336+australiaspolocies@users.noreply.github.com>
2018-06-26 10:38:41 +1000

    Update BGChangingEffect.js

--- API/Scripts/BGChangingEffect.js
+++ API/Scripts/BGChangingEffect.js
@@ -8,6 +8,6 @@ function changeBG()
 {
  while(true)
  {
-  document.body.style.background-color = rgba(BG-color[0]++, BG-color[1]++, BG-color[2]++, 0.3);
+  document.body.style.background-color = rgba(BG-color[0], BG-color[1], BG-color[2], 0.3);
  }
 }
